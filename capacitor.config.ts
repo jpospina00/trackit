31fb7ab439ccc5fea7865752674f03c74a42bd3e
jpospina00 <juan.ospina.vanegas@correounivalle.edu.co>
2025-03-02 17:28:10 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'trackit',
+  webDir: 'www'
+};
+
+export default config;
